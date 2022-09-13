0c5f641dadb0846ea1142c5ffcbb3d621b518eb7
Alan Fung <afung63@my.bcit.ca>
2022-09-12 19:39:50 -0700

    Fixed checkstyle errors

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,10 @@
 package org.comp2522;
 
+/**
+ * Main class containing a method for hello message.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello fellow humans.");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello fellow humans.");
+  }
 }
